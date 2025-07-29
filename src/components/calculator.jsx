c21b438edeb81ca0970c43381e7383bfd66a053e
Raunik Rai <raunik.rai@amptech.corp>
2025-07-29 23:27:55 +0530

    the number going towards right side when overload

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,190 +1,122 @@
-import {useState} from 'react'
-import './calculator.css'
-import { evaluate } from 'mathjs';
-function Calculator(){
-    const [inputValue,setInputNumber] = useState("");
-    const addvalueHandler =(value)=>{
-        setInputNumber((prev)=>prev+value)
+import { useState, useRef, useEffect } from "react";
+import "./calculator.css";
+import { evaluate } from "mathjs";
+function Calculator() {
+  const [inputValue, setInputNumber] = useState("");
+  const displayRef = useRef(null);
+
+  const addvalueHandler = (value) => {
+    setInputNumber((prev) => prev + value);
+  };
+  const answer = () => {
+    try {
+      const result = evaluate(inputValue);
+      setInputNumber(result);
+    } catch (error) {
+      alert("Invalid expression!");
     }
-    const answer = () => {
-        try {
-            const result = evaluate(inputValue);
-            setInputNumber(result);
-        } catch (error) {
-            alert("Invalid expression!");
-        }
-    };
+  };
 
-    return(
-        <>
-            <div className="allcalculator">
-                <input className='display'
-                       value={inputValue}
-                       placeholder="0"
-                       readOnly/>
-                <div className="all-buttons">
-                    <div className="grey" onClick={()=>setInputNumber("")}>AC</div>
-                    <div className="grey" onClick={() => setInputNumber(prev => prev.slice(0, -1))}>C</div>
-                    <div className="grey"  onClick={()=>addvalueHandler("%")}>%</div>
-                    <div className="orange"  onClick={()=>addvalueHandler("/")}><span className='divide'>&divide;</span></div>
+  useEffect(() => {
+    const el = displayRef.current;
+    if (el) {
+      el.scrollLeft = el.scrollWidth;
+    }
+  }, [inputValue]);
+  return (
+    <>
+      <div className="allcalculator">
+        <div
+          ref={displayRef}
+          //   ref={inputRef}
+          //   onFocus={handleFocus}
+          className="hide-scroll display"
+          style={{
+            overflowX: "auto",
+            whiteSpace: "nowrap",
+            textAlign: "right",
+            scrollbarWidth: "none", // for Firefox
+            msOverflowStyle: "none", // for IE/Edge
+          }}
+          placeholder="0"
+        >
+          {inputValue}
+        </div>
 
+        <div className="all-buttons">
+          <div className="grey" onClick={() => setInputNumber("")}>
+            AC
+          </div>
+          <div
+            className="grey"
+            onClick={() => setInputNumber((prev) => prev.slice(0, -1))}
+          >
+            C
+          </div>
+          <div className="grey" onClick={() => addvalueHandler("%")}>
+            %
+          </div>
+          <div className="orange" onClick={() => addvalueHandler("/")}>
+            <span className="divide">&divide;</span>
+          </div>
 
-                    <div className="black" onClick={()=>addvalueHandler(7)}>7</div>
-                    <div className="black" onClick={()=>addvalueHandler(8)}>8</div>
-                    <div className="black" onClick={()=>addvalueHandler(9)}>9</div>
-                    <div className="orange" onClick={()=>addvalueHandler("*")}>X</div>
+          <div className="black" onClick={() => addvalueHandler(7)}>
+            7
+          </div>
+          <div className="black" onClick={() => addvalueHandler(8)}>
+            8
+          </div>
+          <div className="black" onClick={() => addvalueHandler(9)}>
+            9
+          </div>
+          <div className="orange" onClick={() => addvalueHandler("*")}>
+            X
+          </div>
 
+          <div className="black" onClick={() => addvalueHandler(4)}>
+            4
+          </div>
+          <div className="black" onClick={() => addvalueHandler(5)}>
+            5
+          </div>
+          <div className="black" onClick={() => addvalueHandler(6)}>
+            6
+          </div>
+          <div className="orange" onClick={() => addvalueHandler("-")}>
+            -
+          </div>
 
-                    <div className="black" onClick={()=>addvalueHandler(4)}>4</div>
-                    <div className="black" onClick={()=>addvalueHandler(5)}>5</div>
-                    <div className="black" onClick={()=>addvalueHandler(6)}>6</div>
-                    <div className="orange" onClick={()=>addvalueHandler("-")}>-</div>
+          <div className="black" onClick={() => addvalueHandler(1)}>
+            1
+          </div>
+          <div className="black" onClick={() => addvalueHandler(2)}>
+            2
+          </div>
+          <div className="black" onClick={() => addvalueHandler(3)}>
+            3
+          </div>
+          <div className="orange" onClick={() => addvalueHandler("+")}>
+            +
+          </div>
 
-
-                    <div className="black" onClick={()=>addvalueHandler(1)}>1</div>
-                    <div className="black" onClick={()=>addvalueHandler(2)}>2</div>
-                    <div className="black" onClick={()=>addvalueHandler(3)}>3</div>
-                    <div className="orange" onClick={()=>addvalueHandler("+")}>+</div>
-
-                    
-                    <div className="black" onClick={()=>addvalueHandler("00")}>00</div>
-                    <div className="black" onClick={()=>addvalueHandler(0)}>0</div>
-                    <div className="black" onClick={()=>addvalueHandler(".")}>.</div>
-                    <div className="orange" onClick={answer}>=</div>
-                </div>
-            </div>
-        </>
-    )
+          <div className="black" onClick={() => addvalueHandler("00")}>
+            00
+          </div>
+          <div className="black" onClick={() => addvalueHandler(0)}>
+            0
+          </div>
+          <div className="black" onClick={() => addvalueHandler(".")}>
+            .
+          </div>
+          <div className="orange" onClick={answer}>
+            =
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Calculator;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import {useState} from 'react'
 // import {evaluate} from 'mathjs'
@@ -218,11 +150,11 @@ export default Calculator;
 //     }
 //     const answer =()=>{
 //         setValue(evaluate(value))
-        
+
 //     }
 //     return(
 //         <>
-//             <input value={inputNumber} 
+//             <input value={inputNumber}
 //                    onChange={inputNumberHandler}
 //                    type="text"/>
 //             <button onClick={plusAdd} style={{backgroundColor: isclicked === true? 'red': 'white'}}>+</button>
